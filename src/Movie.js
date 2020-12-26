bbf1eccfb378cb6944c3505c711893d5102d1bc3
chiyongs <tlscldyd@gmail.com>
2020-12-27 03:15:04 +0900

    Add movie function

---
+++ src/Movie.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropType from "prop-types";
+
+function Movie({ id, title, year, summary, poster, genres }) {
+  return (
+    <div className="movie">
+      <img src={poster} alt={title} title={title}></img>
+      <div className="movie__date">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p claasName="movies__summary">{summary}</p>
+      </div>
+    </div>
+  );
+}
+
+Movie.PropType = {
+  id: PropType.number.isRequired,
+  title: PropType.string.isRequired,
+  year: PropType.number.isRequired,
+  summary: PropType.string.isRequired,
+  poster: PropType.string.isRequired,
+  genres: PropType.arrayOf(PropType.string).isRequired,
+};
+
+export default Movie;
