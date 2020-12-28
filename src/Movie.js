3d482a5ff79347f9f01c120d2cd15d0574c70f4c
chiyongs <tlscldyd@gmail.com>
2020-12-28 18:57:38 +0900

    Make App.css

--- src/Movie.js
+++ src/Movie.js
@@ -1,11 +1,12 @@
 import React from "react";
 import PropType from "prop-types";
+import "./Movie.css";
 
 function Movie({ id, title, year, summary, poster, genres }) {
   return (
     <div className="movie">
       <img src={poster} alt={title} title={title}></img>
-      <div className="movie__date">
+      <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
         <ul className="genres">
@@ -15,7 +16,7 @@ function Movie({ id, title, year, summary, poster, genres }) {
             </li>
           ))}
         </ul>
-        <p className="movies__summary">{summary}</p>
+        <p className="movie__summary">{summary}</p>
       </div>
     </div>
   );
