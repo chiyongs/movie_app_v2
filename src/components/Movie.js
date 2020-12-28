416a2d615f4773f1abe421d8ae67e63df3e3d572
chiyongs <tlscldyd@gmail.com>
2020-12-29 00:50:04 +0900

    Routing

---
+++ src/components/Movie.js
@@ -0,0 +1,48 @@
+import React from "react";
+import PropType from "prop-types";
+import { Link } from "react-router-dom";
+import "./Movie.css";
+
+function Movie({ id, title, year, summary, poster, genres }) {
+  return (
+    <div className="movie">
+      <Link
+        to={{
+          pathname: `/movie/${id}`,
+          state: {
+            title,
+            year,
+            summary,
+            poster,
+            genres,
+          },
+        }}
+      >
+        <img src={poster} alt={title} title={title}></img>
+        <div className="movie__data">
+          <h3 className="movie__title">{title}</h3>
+          <h5 className="movie__year">{year}</h5>
+          <ul className="genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="genres__genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+          <p className="movie__summary">{summary.slice(0, 180)}...</p>
+        </div>
+      </Link>
+    </div>
+  );
+}
+
+Movie.PropType = {
+  id: PropType.number.isRequired,
+  title: PropType.string.isRequired,
+  year: PropType.number.isRequired,
+  summary: PropType.string.isRequired,
+  poster: PropType.string.isRequired,
+  genres: PropType.arrayOf(PropType.string).isRequired,
+};
+
+export default Movie;
